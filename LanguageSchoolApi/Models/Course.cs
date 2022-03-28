ab32c18cdbff33895db1cff2564f6e46b1b01913
kaue-guerra <kaueguerra@gmail.com>
2022-03-28 00:23:45 -0300

    insert Data Annotations in models Course and Matriculate

--- LanguageSchoolApi/Models/Course.cs
+++ LanguageSchoolApi/Models/Course.cs
@@ -1,11 +1,21 @@
-﻿namespace LanguageSchoolApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LanguageSchoolApi.Models
 {
     public class Course
     {
         public int Id { get; set; }
 
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "O Campo {0} é obrigatorio")]
         public string Name { get; set; }
+
+        [Display(Name = "Número da turma")]
+        [Required(ErrorMessage = "O Campo {0} é obrigatorio")]
         public string NumberClass { get; set; }
+        
+        [Display(Name = "Ano Letivo")]
+        [Required(ErrorMessage = "O Campo {0} é obrigatorio")]
         public DateTime Year { get; set; }
     }
 }
--- LanguageSchoolApi/Models/Matriculate.cs
+++ LanguageSchoolApi/Models/Matriculate.cs
@@ -1,9 +1,18 @@
-﻿namespace LanguageSchoolApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LanguageSchoolApi.Models
 {
     public class Matriculate
     {
         public int Id { get; set; }
+
+        [Display(Name = "CPF")]
+        [Required(ErrorMessage = "O Campo {0} é obrigatorio")]
+        [StringLength(maximumLength: 11, ErrorMessage = "CPF inválido", MinimumLength = 11)]
         public string CpfStudent { get; set; }
+
+        [Display(Name = "Número da turma")]
+        [Required(ErrorMessage = "O Campo {0} é obrigatorio")]
         public string NumberClass { get; set; }
     }
 }
